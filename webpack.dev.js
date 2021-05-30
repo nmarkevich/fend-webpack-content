c58d8269f48f6f9662e5a80e421ad69f2a77768e
nmarkevich <natallia.markevich@gmail.com>
2021-05-30 13:35:46 -0500

    feat: Adding Mode for webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,14 +1,15 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require("path")
+const webpack = require("webpack")
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 
 module.exports = {
+    mode: "development",
     entry: './src/client/index.js',
     devtool: 'source-map',
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: "/\.js$/",
                 exclude: /node_modules/,
                 loader: "babel-loader"
             }
