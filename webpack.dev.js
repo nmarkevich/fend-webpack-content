386b43fbeb1b10b9f6eccb6e6bcf16154399ab66
nmarkevich <natallia.markevich@gmail.com>
2021-05-30 14:08:53 -0500

    feat: Adding bundle analyzer plugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const {CleanWebpackPlugin} = require("clean-webpack-plugin")
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
     entry: './src/client/index.js',
@@ -26,6 +27,7 @@ module.exports = {
             verbose: true, 
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
-        })
+        }),
+        new BundleAnalyzerPlugin()
     ]
 }
